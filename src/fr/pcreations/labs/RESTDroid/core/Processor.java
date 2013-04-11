a9d46dc80886b51903049477966e87c7ccb0d602
Pierre <pcriulan@gmail.com>
2013-04-11 09:34:34 +0200

    Alpha release 0.7.2.5 : See CHANGELOG for details

--- src/fr/pcreations/labs/RESTDroid/core/Processor.java
+++ src/fr/pcreations/labs/RESTDroid/core/Processor.java
@@ -5,9 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.util.Iterator;
 
-import android.util.Log;
 import fr.pcreations.labs.RESTDroid.core.HttpRequestHandler.ProcessorCallback;
 import fr.pcreations.labs.RESTDroid.exceptions.ParsingException;
 import fr.pcreations.labs.RESTDroid.exceptions.PersistableFactoryNotInitializedException;
